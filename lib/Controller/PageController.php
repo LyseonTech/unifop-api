001d40cb15d49cd5c417dd4f3ea32c578c40b8ab
Tevun <setup@tevun.com>
2020-04-29 12:25:09 -0300

    Change app name Change app icon Allow save from all origins

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -60,7 +60,7 @@ class PageController extends Controller {
 	 */
 	public function save() {
 		$response = new JSONResponse();
-		$response->addHeader('Access-Control-Allow-Origin', 'https://lt.coop.br');
+		$response->addHeader('Access-Control-Allow-Origin', '*');
 		$post = $this->request->post;
 		$error = [];
 		if (!isset($post['email']) || !filter_var($post['email'], FILTER_VALIDATE_EMAIL)) {
